80062ddb16a3a9a065c5c5001ddb4d74e9e5b6df
Dipak Padhy <padhydipu005@gmail.com>
2024-02-04 00:11:00 +0530

    Implemented global_bloc.dart for access of current user data throughout the project.

---
+++ lib/global_Bloc/global_event.dart
@@ -0,0 +1,12 @@
+part of 'global_bloc.dart';
+
+@immutable
+abstract class GlobalEvent {}
+
+class GetUserIDEvent extends GlobalEvent {
+  final String? uid;
+
+  GetUserIDEvent({required this.uid});
+}
+
+
---
+++ lib/global_Bloc/global_state.dart
@@ -0,0 +1,12 @@
+part of 'global_bloc.dart';
+
+@immutable
+abstract class GlobalState {}
+
+class GlobalInitial extends GlobalState {}
+
+class GetUserDataFromGlobalBlocState extends GlobalState {
+ final List<UserModel> userData;
+
+  GetUserDataFromGlobalBlocState(this.userData);
+}
