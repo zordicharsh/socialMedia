563e66addefc858a4de180ceaf1950b72578fcde
Dipak Padhy <padhydipu005@gmail.com>
2024-02-05 03:40:57 +0530

    made changes to working of search bar.

--- lib/screens/search_user/searchbloc/search_event.dart
+++ lib/screens/search_user/searchbloc/search_event.dart
@@ -1,5 +1,5 @@
 class Searchevents {}
-
+class SearchUsersIntialEvent extends Searchevents{}
 class SearchTextFieldChangedEvent extends Searchevents{
   String SearchingValue;
   SearchTextFieldChangedEvent(this.SearchingValue);
--- lib/screens/search_user/searchui/searchui.dart
+++ lib/screens/search_user/searchui/searchui.dart
@@ -1,11 +1,7 @@
 import 'dart:developer';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get/get.dart';
-import 'package:socialmedia/model/user_model.dart';
-import 'package:socialmedia/screens/profile/ui/profile.dart';
 import 'package:socialmedia/screens/search_user/searchbloc/search_bloc.dart';
 import 'package:socialmedia/screens/search_user/searchbloc/search_event.dart';
 import 'package:socialmedia/screens/search_user/searchbloc/search_state.dart';
@@ -25,7 +21,7 @@ class _SearchUserState extends State<SearchUser> {
     log("Calling Search widget");
     return Scaffold(
       appBar: AppBar(
-        title: Text('Search Users'),
+        title: const Text('Search Users'),
         backgroundColor: Colors.black,
       ),
       backgroundColor: Colors.black,
@@ -34,38 +30,42 @@ class _SearchUserState extends State<SearchUser> {
           padding: const EdgeInsets.all(10),
           child: Column(
             children: [
-              BlocListener<SearchBloc, SearchState>(
-                listener: (context, state) {
-
-                },
-                child: TextField(
-                  controller: search,
-                  onChanged: (value) {
+              TextField(
+                controller: search,
+                onChanged: (value) {
+                  if (value.isEmpty) {
+                    search.clear();
+                    BlocProvider.of<SearchBloc>(context)
+                        .add(SearchUsersIntialEvent());
+                  } else if (value.length == 1) {
+                    BlocProvider.of<SearchBloc>(context)
+                        .add(SearchUsersIntialEvent());
+                  } else {
                     BlocProvider.of<SearchBloc>(context)
                         .add(SearchTextFieldChangedEvent(value));
-                  },
-                  style: TextStyle(color: Colors.white),
-                  cursorColor: Colors.white,
-                  decoration: InputDecoration(
-                    labelText: "Enter a username",
-                    labelStyle: TextStyle(color: Colors.white),
-                    prefixIcon: Icon(Icons.search, color: Colors.white),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
+                  }
+                },
+                style: const TextStyle(color: Colors.white),
+                cursorColor: Colors.white,
+                decoration: InputDecoration(
+                  labelText: "Enter a username",
+                  labelStyle: const TextStyle(color: Colors.white),
+                  prefixIcon: const Icon(Icons.search, color: Colors.white),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.white),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.white),
                   ),
                 ),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               Expanded(
                 child: BlocBuilder<SearchBloc, SearchState>(
                   builder: (context, state) {
@@ -86,28 +86,34 @@ class _SearchUserState extends State<SearchUser> {
                                 maxRadius: 50,
                                 minRadius: 20,
                                 backgroundColor: Colors.grey,
-                                backgroundImage: NetworkImage(state.users[index].Profileurl.toString()),
+                                backgroundImage: NetworkImage(
+                                    state.users[index].Profileurl.toString()),
                               ),
                               title: Text(
                                 state.users[index].Username.toString(),
-                                style: TextStyle(color: Colors.white),
+                                style: const TextStyle(color: Colors.white),
                               ),
                               subtitle: Text(
                                 state.users[index].Uid.toString(),
-                                style: TextStyle(color: Colors.white70),
+                                style: const TextStyle(color: Colors.white70),
                               ),
                               // contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                             ),
                           );
                         },
                       );
-                    }else if(state is NouserAvailable){
+                    } else if (state is NouserAvailable) {
                       print("nouseravailable");
-                        return Center(child: Container(child: Text("no user found"),));
-                    }
-                    else {
+                      return Center(
+                          child: Container(
+                        child: const Text("no user found"),
+                      ));
+                    } else {
                       print("inint");
-                      return Center(child: Container(child: Text("init state"),));
+                      return Center(
+                          child: Container(
+                        child: const Text("init state"),
+                      ));
                     }
                   },
                 ),
